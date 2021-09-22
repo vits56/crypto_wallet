28e1a0b9da4259498715cff1315cf051597e2557
Bruno Bastos <bastosduartebruno@gmail.com>
2021-09-22 09:27:23 -0300

    fixed error on scaffold

--- app/views/coins/_coin.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! coin, :id, :description, :created_at, :updated_at
-json.url coin_url(coin, format: :json)
--- db/migrate/20210922115446_create_coins.rb
+++
@@ -1,9 +0,0 @@
-class CreateCoins < ActiveRecord::Migration[5.2]
-  def change
-    create_table :coins do |t|
-      t.string :description
-
-      t.timestamps
-    end
-  end
-end
