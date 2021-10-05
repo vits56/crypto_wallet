e1223202cbd36bdb4630aabf12cab87da5ed0a3f
Bruno Bastos <bastosduartebruno@gmail.com>
2021-10-05 01:26:25 -0300

    refatorado o seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,31 @@
-puts "Cadastrando moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas... ")
+spinner.auto_spin
+spinner.success("(Moedas cadastradas com sucesso!)")
 
-Coin.create!(
+
+coins = [
+  {
     description: "Bitcoin",
     acronym: "BTC",
     url_image: "https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg"
-)
+  },
 
-Coin.create!(
+  {
     description: "Ethereum",
     acronym: "ETH",
     url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZCRfwkqpPvFb3QmmwGONG2i6PsgnqZ3L7dRzCNlaSTB1-ruu5"
-)
+  },
 
-Coin.create!(
+  {
     description: "Dash",
     acronym: "DASH",
     url_image: "https://ih1.redbubble.net/image.406055498.8711/ap,550x550,12x12,1,transparent,t.png"
-)
+  }
+]
 
-puts "Moedas cadastradas com sucesso!"
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+
+spinner.success("(Moedas cadastradas com sucesso!)")
